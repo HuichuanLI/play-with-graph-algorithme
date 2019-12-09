e02c1fe706e67508411c0ed8472600053239ff0b
huichuan <lhc14124908@163.com>
2019-12-09 09:58:47 +0800

    add

--- src/Chapter02GraphDFS/Graph.java
+++ src/Chapter02GraphDFS/Graph.java
@@ -1,19 +1,19 @@
-package Chapter01Graph_Basics;
+package Chapter02GraphDFS;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.LinkedList;
 import java.util.TreeSet;
+import java.util.Scanner;
 
-// 无向无权图
-public class AdjSet {
+
+/// 暂时只支持无向无权图
+public class Graph {
+
     private int V;
     private int E;
     private TreeSet<Integer>[] adj;
 
-    public AdjSet(String filename) {
+    public Graph(String filename) {
 
         File file = new File(filename);
 
@@ -23,7 +23,7 @@ public class AdjSet {
             if (V < 0) throw new IllegalArgumentException("V must be non-negative");
             adj = new TreeSet[V];
             for (int i = 0; i < V; i++)
-                adj[i] = new TreeSet<>();
+                adj[i] = new TreeSet<Integer>();
 
             E = scanner.nextInt();
             if (E < 0) throw new IllegalArgumentException("E must be non-negative");
@@ -33,9 +33,8 @@ public class AdjSet {
                 validateVertex(a);
                 int b = scanner.nextInt();
                 validateVertex(b);
-                // 没有自环边
+
                 if (a == b) throw new IllegalArgumentException("Self Loop is Detected!");
-                // 没有平行边
                 if (adj[a].contains(b)) throw new IllegalArgumentException("Parallel Edges are Detected!");
 
                 adj[a].add(b);
@@ -44,6 +43,11 @@ public class AdjSet {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IllegalArgumentException("vertex " + v + "is invalid");
     }
 
     public int V() {
@@ -65,7 +69,6 @@ public class AdjSet {
         return adj[v];
     }
 
-
     public int degree(int v) {
         validateVertex(v);
         return adj[v].size();
@@ -76,24 +79,18 @@ public class AdjSet {
         StringBuilder sb = new StringBuilder();
 
         sb.append(String.format("V = %d, E = %d\n", V, E));
-        for (int i = 0; i < V; i++) {
-            sb.append(String.format("%d : ", i));
-            for (int w : adj[i])
+        for (int v = 0; v < V; v++) {
+            sb.append(String.format("%d : ", v));
+            for (int w : adj[v])
                 sb.append(String.format("%d ", w));
             sb.append('\n');
         }
         return sb.toString();
     }
 
-    private void validateVertex(int v) {
-        if (v < 0 || v >= V)
-            throw new IllegalArgumentException("vertex " + v + "is invalid");
-    }
-
     public static void main(String[] args) {
 
-        AdjSet AdjSet = new AdjSet("/Users/hui/Desktop/java/play-with-graph-algorithme/src/Chapter01Graph_Basics/g.txt");
-        System.out.print(AdjSet);
+        Graph g = new Graph("/Users/hui/Desktop/java/play-with-graph-algorithme/src/Chapter02GraphDFS/g.txt");
+        System.out.print(g);
     }
-
 }
