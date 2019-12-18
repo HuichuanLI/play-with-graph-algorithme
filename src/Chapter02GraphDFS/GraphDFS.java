96421794dee46cd23548d53a9f0767ff430a9ef3
huichuan <lhc14124908@163.com>
2019-12-18 10:00:50 +0800

    add

--- src/Chapter02GraphDFS/GraphDFS.java
+++ src/Chapter02GraphDFS/GraphDFS.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 public class GraphDFS {
     private ArrayList<Integer> order = new ArrayList<>();
+    private ArrayList<Integer> post = new ArrayList<>();
+
     private Graph G;
     private boolean[] visited;
 
@@ -12,7 +14,10 @@ public class GraphDFS {
 
         this.G = G;
         visited = new boolean[G.V()];
-        dfs(0);
+        for (int v = 0; v < G.V(); v++) {
+            if (!visited[v])
+                dfs(v);
+        }
     }
 
     private void dfs(int v) {
@@ -22,17 +27,24 @@ public class GraphDFS {
         for (int w : G.adj(v))
             if (!visited[w])
                 dfs(w);
+
+        post.add(v);
     }
 
-    public Iterable<Integer> order() {
+    public Iterable<Integer> pre() {
         return order;
+    }
+
+    public Iterable<Integer> post() {
+        return post;
     }
 
     public static void main(String[] args) {
 
         Graph g = new Graph("/Users/hui/Desktop/java/play-with-graph-algorithme/src/Chapter02GraphDFS/g.txt");
         GraphDFS graphDFS = new GraphDFS(g);
-        System.out.println(graphDFS.order());
+        System.out.println("DFS preOrder : " + graphDFS.pre());
+        System.out.println("DFS postOrder : " + graphDFS.post());
     }
 
 }
