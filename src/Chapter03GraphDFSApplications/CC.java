f5d996eecfd45dc06e244551adddba27d198974a
huichuan <lhc14124908@163.com>
2019-12-20 00:13:25 +0800

    add

--- src/Chapter03GraphDFSApplications/CC.java
+++ src/Chapter03GraphDFSApplications/CC.java
@@ -1,38 +1,77 @@
 package Chapter03GraphDFSApplications;
 
+import java.util.ArrayList;
+
+// 求解图中的连通分量
+// 和每个联通分量的变量
 public class CC {
 
     private Graph G;
-    private boolean[] visited;
+    private int[] visited;
     private int cccount = 0;
 
-    public CC(Graph G){
+    public CC(Graph G) {
 
         this.G = G;
-        visited = new boolean[G.V()];
-        for(int v = 0; v < G.V(); v ++)
-            if(!visited[v]){
-                dfs(v);
-                cccount ++;
+        visited = new int[G.V()];
+        for (int i = 0; i < visited.length; i++)
+            visited[i] = -1;
+
+        for (int v = 0; v < G.V(); v++)
+            if (visited[v] == -1) {
+                dfs(v, cccount);
+                cccount++;
             }
     }
 
-    private void dfs(int v){
+    private void dfs(int v, int cccount) {
 
-        visited[v] = true;
-        for(int w: G.adj(v))
-            if(!visited[w])
-                dfs(w);
+        visited[v] = cccount;
+        for (int w : G.adj(v))
+            if (visited[w] == -1)
+                dfs(w, cccount);
     }
 
-    public int count(){
+    public boolean isConnected(int v, int w) {
+        G.validateVertex(v);
+        G.validateVertex(w);
+        return visited[v] == visited[w];
+    }
+
+    public int count() {
+        for (int e : visited)
+            System.out.print(e + " ");
+        System.out.println();
         return cccount;
     }
 
-    public static void main(String[] args){
+    public ArrayList<Integer>[] components() {
+
+        ArrayList<Integer>[] res = new ArrayList[cccount];
+        for (int i = 0; i < cccount; i++)
+            res[i] = new ArrayList<Integer>();
+
+        for (int v = 0; v < G.V(); v++)
+            res[visited[v]].add(v);
+        return res;
+    }
+
+
+    public static void main(String[] args) {
 
         Graph g = new Graph("/Users/hui/Desktop/java/play-with-graph-algorithme/src/Chapter03GraphDFSApplications/g.txt");
         CC cc = new CC(g);
         System.out.println(cc.count());
+
+        System.out.println(cc.isConnected(0, 6));
+        System.out.println(cc.isConnected(5, 6));
+
+        ArrayList<Integer>[] comp = cc.components();
+        for (int ccid = 0; ccid < comp.length; ccid++) {
+            System.out.print(ccid + " : ");
+            for (int w : comp[ccid])
+                System.out.print(w + " ");
+            System.out.println();
+        }
     }
 }
--- src/Chapter03GraphDFSApplications/Graph.java
+++ src/Chapter03GraphDFSApplications/Graph.java
@@ -46,7 +46,7 @@ public class Graph {
         }
     }
 
-    private void validateVertex(int v){
+    public void validateVertex(int v){
         if(v < 0 || v >= V)
             throw new IllegalArgumentException("vertex " + v + "is invalid");
     }
