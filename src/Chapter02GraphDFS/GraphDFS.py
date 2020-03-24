99d002e9b9885d5a55f5badc259eff9d6d9735da
huichuan <lhc14124908@163.com>
2020-03-24 10:54:30 +0800

    add

--- src/Chapter02GraphDFS/GraphDFS.py
+++ src/Chapter02GraphDFS/GraphDFS.py
@@ -1,4 +1,6 @@
 from adjSet import adjSet
+
+
 class GraphDFS:
     def __init__(self, G):
         self._pre_order = []
@@ -10,7 +12,8 @@ class GraphDFS:
     def dfs(self):
         for i in range(self._G.V):
             if not self._visited[i]:
-                self._dfs(i)
+                self._visited[i] = True
+                self._dfs_iteration(i)
 
     def _dfs(self, root):
         self._visited[root] = True
@@ -19,6 +22,19 @@ class GraphDFS:
             if not self._visited[w]:
                 self._dfs(w)
         self._post_order.append(root)
+
+    def _dfs_iteration(self, root):
+        stack = [root]
+        stack2 = []
+        while len(stack) > 0:
+            cur = stack.pop()
+            stack2.append(cur)
+            self._pre_order.append(cur)
+            for w in self._G.adj(cur):
+                if not self._visited[w]:
+                    stack.append(w)
+                    self._visited[w] = True
+        self._post_order += stack2[::-1]
 
     @property
     def pre_order(self):
@@ -30,7 +46,7 @@ class GraphDFS:
 
 
 if __name__ == '__main__':
-    print('For one block, recursive:')
+    # print('For one block, recursive:')
     filename = '/Users/hui/Desktop/java/play-with-graph-algorithme/src/Chapter02GraphDFS/g.txt'
     g = adjSet(filename)
     graph_dfs = GraphDFS(g)
@@ -39,8 +55,8 @@ if __name__ == '__main__':
 
     print('*' * 40)
 
-    print('For two blocks, recursive:')
-    filename = '/Users/hui/Desktop/java/play-with-graph-algorithme/src/Chapter02GraphDFS/g.txt'
+    # print('For two blocks, recursive:')
+    filename = '/Users/hui/Desktop/java/play-with-graph-algorithme/src/Chapter02GraphDFS/g2.txt'
     g = adjSet(filename)
     graph_dfs = GraphDFS(g)
     print(graph_dfs.pre_order)
